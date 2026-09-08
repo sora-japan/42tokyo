1e6662693d1f65f4529b01b388cb3ddb18c9df3c
sora-japan <takayuki555ft@gmail.com>
2026-09-09 06:20:33 +0900

    ex4完了

---
+++ circle_2/python_module/python_module02/ex4/ft_finally_block.py
@@ -0,0 +1,38 @@
+#!/usr/bin/env python3
+class GardenError(Exception):
+    def __init__(self, message="Unknown garden error"):
+        super().__init__(message)
+
+
+class PlantError(GardenError):
+    def __init__(self, message="Unknown plant error"):
+        super().__init__(message)
+
+
+def water_plant(plant_name):
+    if plant_name.capitalize() == plant_name:
+        print(f"Watering {plant_name}: [OK]")
+    else:
+        raise PlantError(f"Invalid plant name to water:'{plant_name}'")
+
+def test_watering_system(plant_names):
+    print("Opening watering system")
+    try:
+        for plant in plant_names:
+            water_plant(plant)
+    except PlantError as e:
+        print(f"Caught PlantError: {e}")
+        print(".. ending tests and returning to main")
+        return
+    finally:
+        print("Closing watering system")
+
+if __name__ == "__main__":
+    print("=== Garden Watering System ===\n")
+    print("Testing valid plants...")
+    test_watering_system(["Tomato", "Lettuce", "Carrots"])
+
+    print("\nTesting invalid plants...")
+    test_watering_system(["Tomato", "lettuce"])
+
+    print("\nCleanup always happens, even with errors!")
